b6fadf76cbd10831da5494c92e8a790403947172
suryansidhal <bhabanidhal1980@gmail.com>
2025-06-24 17:46:57 +0530

    Add files via upload
    THE FIRST DAY SEEMS RIGHT.

---
+++ hello.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  
+  int age=35;
+  
+  char name ='A';
+ 
+  float PIE = 3.14f;
+  
+  bool IsGood= true or false;
+
+  int takingITaway=name;
+   
+  int sayAGE;
+    
+  cout<< "please write your age for security purpose;";
+   
+  cin>> sayAGE;
+    
+  int fatherAGE;
+    
+  cout<< "please write your father's age for security purpose;";
+      cin>> fatherAGE;
+  
+  cout<<"here's the summation you initiated:" << (sayAGE+fatherAGE)<<"\n"<< "along with remainder and quotient respectively;"<< (sayAGE% fatherAGE)<<"   " << "AND  "<< (sayAGE / (double)fatherAGE)<<"\n";
+
+    
+  cout << "your are doing well \n"<< sizeof(age)<<"\n" <<age<<"\n"<< sizeof(PIE)<<"\n"<< PIE;
+    
+    
+  
+
+  return 0;
+}
