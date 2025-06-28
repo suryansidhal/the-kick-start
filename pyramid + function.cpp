d3413eef95620fefe0d0c76aab08ae4f254ca91c
suryansidhal <bhabanidhal1980@gmail.com>
2025-06-28 21:07:30 +0530

    Add files via upload
    The pattern went nasty by the end, especially the hollow diamond. FUNCTIONS seems good but a bit trickier as compared to python's function. Looking forward to any suggestions on my codes .

---
+++ pyramid + function.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+int main(){
+    int n=4;
+    
+    for(int i=0;i<n;i++){
+        int j=1;
+        for(int e=3-i;e>0;e--){
+            cout<<" ";
+        }
+        for(int w=0;w<i+1;w++){
+            cout<<j++;
+    
+        
+        }
+        for(int d=i;d>0;d--){
+            cout<<d;
+        }
+        cout<<"\n";
+  
+// fun for prime or not
+#include <iostream>
+using namespace std;
+bool IsPrime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+            
+        }else{
+            
+            return true;
+        }
+           }
+    }
+          
